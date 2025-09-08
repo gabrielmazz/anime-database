1def5acddce4266c6793749caa8086063f0a67cd
gabrielmazz <gabrielalvesmazzuco@gmail.com>
2025-09-07 22:11:11 -0300

    NS01 - Commit incial do projeto do anime database

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+
+import { BrowserRouter } from 'react-router-dom'
+import { createTheme, MantineProvider } from '@mantine/core';
+
+import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
+
+// Importação das telas
+import SelectionScreen from './screens/ selectionScreen.tsx'
+import SearchScreen from './screens/searchAnime.tsx'
+
+createRoot(document.getElementById('root')!).render(
+
+	<StrictMode>
+		<MantineProvider>
+
+			<BrowserRouter basename="/selectionScreen">
+				<SelectionScreen />
+			</BrowserRouter>
+
+			<BrowserRouter basename="/searchScreen">
+				<SearchScreen />
+			</BrowserRouter>
+
+
+		</MantineProvider>
+
+	</StrictMode>,
+)
